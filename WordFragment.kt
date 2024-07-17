f02866fd680b01dbbc1c96bbd5ff48bb6acc5de3
filiksyos <126945152+filiksyos@users.noreply.github.com>
2024-07-17 12:54:49 +0300

    Update WordFragment.kt

--- WordFragment.kt
+++ WordFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.localstealcon.DialogFragments.AddVictimDialog
+import android.widget.Button
 import com.example.localstealcon.DialogFragments.AddWordDialog
 import com.example.localstealcon.R
 
@@ -15,11 +15,16 @@ class WordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_word, container, false)
-
-        val addWordDialog =  AddWordDialog()
-        addWordDialog.show(childFragmentManager, "add_victim_dialog")
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val addWordButton = view.findViewById<Button>(R.id.addWordFab)
+        addWordButton.setOnClickListener {
+            val addWordDialog = AddWordDialog()
+            addWordDialog.show(childFragmentManager, "add_word_dialog")
+        }
+    }
 }
