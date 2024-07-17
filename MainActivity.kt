b6d50fe1e809f838acb3602c44e4547757a80bba
filiksyos <126945152+filiksyos@users.noreply.github.com>
2024-07-17 12:53:16 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,1 +1,20 @@
+package com.example.localstealcon
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.localstealcon.ViewModel.VictimViewModel
+import com.example.localstealcon.ViewModel.WordViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var victimViewModel: VictimViewModel
+    lateinit var wordViewModel:   WordViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
