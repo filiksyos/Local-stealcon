e514cb97af732ac490e4db28e4b58a72daeb9981
filiksyos <126945152+filiksyos@users.noreply.github.com>
2024-07-18 13:21:10 +0300

    Update VictimEntity.kt

--- VictimEntity.kt
+++ VictimEntity.kt
@@ -1,1 +1,17 @@
+package com.example.localstealcon.Room
 
+import androidx.room.*
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "Victims")
+@Parcelize
+data class VictimEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "VictimName")
+    val victimName: String,
+    // Add other properties as needed, e.g.,
+    // @ColumnInfo(name = "email")val email: String?
+    // @ColumnInfo(name = "phone") val phoneNumber: String?
+): Parcelable
