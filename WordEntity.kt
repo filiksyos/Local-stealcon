7c45a7b02527222be3b9898a7ee76ed9c910caff
filiksyos <126945152+filiksyos@users.noreply.github.com>
2024-07-18 13:22:03 +0300

    Update WordEntity.kt

--- WordEntity.kt
+++ WordEntity.kt
@@ -1,1 +1,18 @@
+package com.example.localstealcon.Room
+
+import androidx.room.*
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "Words")
+@Parcelize
+data class WordEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "WordName")
+    val wordName: String,
+    // Add other properties as needed, e.g.,
+    // @ColumnInfo(name = "email")val email: String?
+    // @ColumnInfo(name = "phone") val phoneNumber: String?
+): Parcelable
 
